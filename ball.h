0e0082a0875caa42f4898275f2a3a8250d891832
MusserO <iiro.kumpulainen@netikka.fi>
2014-08-23 12:05:05 +0300

    Pallo liikkuu
    O

--- ball.h
+++ ball.h
@@ -1,3 +1,6 @@
+
+#include "SFML/include/SFML/System/Vector2.hpp"
+
 #define PI 3.141592653589793
 class Ball
 {
@@ -13,6 +16,7 @@ class Ball
 		sf::Sound sound;
 		sf::CircleShape shape;
 		float angle;
+                float speed;
 
 };
 Ball::Ball(sf::SoundBuffer SB) {
@@ -26,6 +30,7 @@ Ball::Ball(sf::SoundBuffer SB) {
 	shape.setFillColor(sf::Color::Blue);
 	shape.setOrigin(radius / 2, radius / 2);
 
+        speed = 200.f;
 	angle = (std::rand() % 360) * 2 * PI / 360;
 	while (std::abs(std::cos(angle)) < 0.7f) {
 		angle = (std::rand() % 360) * 2 * PI / 360;
@@ -36,8 +41,9 @@ Ball::~Ball() {}
 void Ball::setPos(sf::Vector2f v) {
 	shape.setPosition(v);
 }
-void Ball::move(float a) {
-
+void Ball::move(float dT) {
+    shape.move(dT*cos(angle)*speed,dT*cos(angle)*speed);
+    speed += 0.1f;
 }
 void Ball::draw(sf::RenderWindow& window) {
 	window.draw(shape);
